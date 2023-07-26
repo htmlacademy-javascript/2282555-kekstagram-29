2b38214a24b716e8b56d4fd628236889785ba6d5
Микляева Ева <miklyayeva09@mail.ru>
2023-07-26 15:31:59 +0300

    Удаляет лишние функции

--- js/util.js
+++ js/util.js
@@ -1,5 +1,4 @@
-//const getRandomInteger = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
-//const getArrayElement = (array) => array[getRandomInteger (0, array.length - 1)];
+
 const isEscapeKey = (evt) => evt.key === 'Escape';
 const isNotInput = (event) => !event.target.closest('input') || !event.target.closest('textarea');
 
