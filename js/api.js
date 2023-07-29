250f7ba4893d45b634d5c6cc6316528856a0075d
Микляева Ева <miklyayeva09@mail.ru>
2023-07-29 23:05:35 +0300

    Правки от проверяющего

--- js/api.js
+++ js/api.js
@@ -1,38 +1,32 @@
 const DEFAULT_METHOD = 'POST';
 
-const getData = (url, onSuccess, onError) => {
+const getData = (url, onSuccess, onError) =>
   fetch(url)
     .then((response) => {
       if (response.ok) {
         return response.json();
       }
-      throw new Error(`${response.status} ${response.statusText}`);
-    })
-    .then((data) => {
-      onSuccess(data);
-    })
-    .catch((err) => {
-      onError(err);
-    });
-};
 
-const sendData = (url, body, onSuccess, onError, method = DEFAULT_METHOD) => {
-  fetch(url, {
-    method,
-    body,
-  })
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      }
-      throw new Error(`${response.status} ${response.statusText}`);
+      throw new Error();
     })
-    .then(() => {
+    .then((data) => onSuccess(data))
+    .catch((error) => onError(error));
+
+const sendData = (url, body, onSuccess, onError, method = DEFAULT_METHOD) =>
+  fetch(
+    url, {
+      method,
+      body,
+    })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error();
+      }
+
       onSuccess();
     })
-    .catch((err) => {
-      onError(err);
+    .catch(() => {
+      onError();
     });
-};
 
 export { getData, sendData };
